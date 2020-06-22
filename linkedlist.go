d3bd85c56c473806a420be63340627690ee6b3e5
buglan <1831353087@qq.com>
2020-06-22 18:54:55 +0800

    feat: linkedlist

---
+++ linkedlist.go
@@ -0,0 +1,22 @@
+package algorithm
+
+type ListNode struct {
+	value interface{}
+	next  *ListNode
+}
+
+func (l *ListNode) Push(value interface{}) {
+
+}
+
+func (l *ListNode) Pop() (value interface{}, err error) {
+
+}
+
+func (l *ListNode) Peek() (value interface{}, err error) {
+
+}
+
+func (l *ListNode) Reverse() *ListNode {
+
+}
