f934bfa2d046da174ad7f96cda4d718376782147
buglan <1831353087@qq.com>
2020-08-01 12:24:58 +0800

    fix: binary tree

--- binarytree.go
+++ binarytree.go
@@ -2,29 +2,38 @@ package algorithm
 
 // TreeNode 二叉树节点
 type TreeNode struct {
-	Val   interface{}
+	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-// NewTreeNode create tree node
-func NewTreeNode(val interface{}) *TreeNode {
-	return &TreeNode{
-		Val: val,
+// PreOrder 前序遍历: 先遍历根节点, 再遍历左子树
+func PreOrder(root *TreeNode, list *[]int) {
+	if root == nil {
+		return
 	}
+	*list = append(*list, root.Val)
+	PreOrder(root.Left, list)
+	PreOrder(root.Right, list)
 }
 
-// PreOrder 前序遍历
-func (root *TreeNode) PreOrder() {
-
+// InOrder 中序遍历: 先遍历左子树, 再遍历根节点, 再遍历左子树
+func InOrder(root *TreeNode, list *[]int) {
+	if root == nil {
+		return
+	}
+	InOrder(root.Left, list)
+	*list = append(*list, root.Val)
+	InOrder(root.Right, list)
 }
 
-// InOrder 中序遍历
-func (root *TreeNode) InOrder() {
+// BackOrder 后序遍历: 先遍历左子树, 再遍右子树, 再遍历根
+func BackOrder(root *TreeNode, list *[]int) {
+	if root == nil {
+		return
+	}
 
-}
-
-// BackOrder 后序遍历
-func (root *TreeNode) BackOrder() {
-
+	BackOrder(root.Left, list)
+	BackOrder(root.Right, list)
+	*list = append(*list, root.Val)
 }
--- binarytree_test.go
+++ binarytree_test.go
@@ -2,8 +2,69 @@ package algorithm
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNewTreeNode(t *testing.T) {
+func TestPreOrder(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}
 
+	list := make([]int, 0)
+	PreOrder(root, &list)
+	assert.Equal(t, 3, len(list))
+	assert.Equal(t, []int{1, 2, 3}, list)
+}
+
+func TestInOrder(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+
+	list := make([]int, 0)
+	InOrder(root, &list)
+	assert.Equal(t, 3, len(list))
+	assert.Equal(t, []int{2, 1, 3}, list)
+}
+
+func TestBackOrder(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+
+	list := make([]int, 0)
+	BackOrder(root, &list)
+	assert.Equal(t, 3, len(list))
+	assert.Equal(t, []int{2, 3, 1}, list)
 }
